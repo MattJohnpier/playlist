16cd5abe1e56fa13882a9d5f49d43597a8828d76
matthew johnpier <mattjohnpier@gmail.com>
2019-01-16 23:11:43 +0000

    add artists

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 /* global $ */
 
 // BELOW Update the songs array with four of your favorites songs.
-var songs = [];
+var songs = ["alien boy","still feel","new girl","queen","conquest"];
+var songImg = ["https://bit.ly/2Cr2mB7","https://bit.ly/2stxueX","https://bit.ly/2FHGu89","https://bit.ly/2CobI0E","https://bit.ly/2W0oOua"];
+var songArtist = ["alienArt","StillArt","newArt","queenArt","conArt"];
+var songLength = ["alienTime","StillTime","newTime","queen"]
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
