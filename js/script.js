a88872df9bbe86c5cacdc823348b6bd514c72a17
matthew johnpier <mattjohnpier@gmail.com>
2019-02-06 22:28:55 +0000

    done

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ var songs = ["alien boy","still feel","new girl","queen","conquest"];
 var songImg = ["https://bit.ly/2Cr2mB7","https://bit.ly/2stxueX","https://bit.ly/2FHGu89","https://bit.ly/2CobI0E","https://bit.ly/2W0oOua"];
 var songArtist = ["Oliver Tree","half alive","FINNEAS","Queen","The White Stripes"];
 var songLength = ["2:44","4:15","2:59","4:23","3:47"];
-var songLink = ["","","","",""];
+var songLink = ["https://bit.ly/2WMpBz1","https://bit.ly/2vXUXGc","https://bit.ly/2DVcaoX","https://bit.ly/1hbuBoJ","https://bit.ly/1N85HTV"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
@@ -31,22 +31,34 @@ function displaySongInfo(){
     });
     
     songLink.forEach(function(link) {
-        $("links").append("<p>"+link+"</p>");
+        $("#links").append("<a href ='"+link+"'> Link to Song</a>");
     });
 }
 
 function emptySongInfo(){
     $("#songs").empty();
     // Use jQuery to empty all of the remaining divs
-
+    $("#images").empty();
+    $("#artists").empty();
+    $("#lengths").empty();
+    $("#links").empty();
 
 }
 
 
 function addSongInfo(){
     // BELOW write the code to add new items to each of the arrays.
+    var song = $("#song").val();
+    var image = $("#image").val();
+    var length = $("#length").val();
+    var artist = $("#artist").val();
+    var link = $("#link").val();
     
-
+    songs.push(song);
+    songImg.push(image);
+    songArtist.push(artist);
+    songLength.push(length);
+    songLink.push(link);
 }
 
 $("#add").click(function() {
@@ -55,4 +67,4 @@ $("#add").click(function() {
     displaySongInfo();
 });
 
-//displaySongInfo();
+displaySongInfo();
